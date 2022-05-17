d53a7a8f37c791fea6d4befea26937da5d2d0599
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-05-17 11:16:32 +0100

    Cleanup error handling

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const {
   getOneArticle,
   updateArticleWithVotes,
 } = require("./controllers/controller.js");
+const {
+  handlePSQLErrors,
+  handleCustomErrors,
+  handleInternalServerError,
+} = require("./controllers/errors.controller.js");
 
 const app = express();
 app.use(express.json());
@@ -21,23 +26,8 @@ app.use("/*", (req, res, next) => {
   res.status(404).send({ msg: "Path not found" });
 });
 
-app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Bad Request" });
-  } else {
-    next(err);
-  }
-});
-app.use((err, req, res, next) => {
-  if (err) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    next(err);
-  }
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).send({ msg: "Internal server error" });
-});
+app.use(handlePSQLErrors);
+app.use(handleCustomErrors);
+app.use(handleInternalServerError);
 
 module.exports = app;
